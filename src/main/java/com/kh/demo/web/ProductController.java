a5ebaddac3e2c5b1b03a64535d28aca338202854
sanghak <sang105kr@gmail.com>
2024-11-01 14:26:11 +0900

    20241101-5

--- src/main/java/com/kh/demo/web/ProductController.java
+++ src/main/java/com/kh/demo/web/ProductController.java
@@ -10,10 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
@@ -146,10 +143,10 @@ public class ProductController {
 
   //여러건 삭제처리
   @PostMapping("/del")
-  public String deleteByIds(List<Long> productIds){
+  public String deleteByIds(@RequestParam("productIds") List<Long> productIds){
     log.info("productIds={}", productIds);
 
-//    int rows = productSVC.deleteByIds(productIds);
+    int rows = productSVC.deleteByIds(productIds);
 
     return "redirect:/products";   // 302 get redirectUrl->http://localhost:9080/products
   }
